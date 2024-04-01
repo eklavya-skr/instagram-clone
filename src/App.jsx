c5efe53a9ffe93e54a5841cca1a1bda0c1924110
eklavya-skr <hesedskr2022@gmail.com>
2024-04-02 01:56:43 +0530

    created the homepage and auth file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import { Button } from "@chakra-ui/react";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AuthPage from "./pages/AuthPage";
 function App() {
-  return <>hi</>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/auth" element={<AuthPage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/AuthPage.jsx
@@ -0,0 +1,11 @@
+import { Container, Flex } from "@chakra-ui/react";
+
+const AuthPage = () => {
+  return (
+    <Flex minH={"100vh"} justifyContent={"center"} alignItems={"center"} px={4}>
+      <Container maxW={"container.md"} padding={0}></Container>
+    </Flex>
+  );
+};
+
+export default AuthPage;
